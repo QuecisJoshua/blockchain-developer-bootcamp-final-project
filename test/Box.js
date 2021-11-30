de6ebb0e4681646fbcda01d6b61ffde19b9d4497
Quecis <55006890+QuecisJoshua@users.noreply.github.com>
2021-11-30 11:49:44 -0700

    Add files via upload

---
+++ test/Box.js
@@ -0,0 +1,35 @@
+const { expect } = require('chai');
+
+// Start test block
+describe('WavePortal', function () {
+  before(async function () {
+    this.wavePortal = await ethers.getContractFactory('WavePortal');
+  });
+
+  beforeEach(async function () {
+
+  });
+
+  // Test case
+  it('retrieve returns a value previously stored', async function () {
+    
+  });
+
+  it('verifies the address', async function () {
+    
+  });
+
+  it('storing the message', async function () {
+    
+});
+
+    it('verifying winner on the contract', async function () {
+    
+});
+
+
+    it('get the total number of waves', async function () {
+    
+});
+
+});
