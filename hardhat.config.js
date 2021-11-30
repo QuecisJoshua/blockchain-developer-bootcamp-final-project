8df6828979bb1f0e271dc001441a1e21f50db6b8
TQ45UT <quecis25@gmail.com>
2021-11-30 09:47:01 -0700

    V

---
+++ hardhat.config.js
@@ -0,0 +1,23 @@
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+require('@nomiclabs/hardhat-waffle');
+
+module.exports = {
+  solidity: '0.8.0',
+  networks: {
+    rinkeby: {
+      url: 'https://eth-rinkeby.alchemyapi.io/v2/ka8S46CXq-bQJjpZu-lXUl92rZJj2ZTz',
+      accounts: ['de26dacdea320107dd6a5bc503e38fca12124df4443a63b71da440d17ef53b52'],
+    },
+  },
+};
